06b0faa448e968cbe34e502b6a82fcd8b362e19e
Izura <uchihaizura1219@gmail.com>
2022-05-23 22:15:23 +0300

    version 2.0
    people human

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ let getRand = (min,max) => {
 //массивы со свойствами
 const stylesModes = ['overlay', 'soft-light', 'hard-light', 'darken', 'multiply', 'color-burn', 'lighten', 'screen', 'color-dodge', 'difference', 'exclusion', 'hue', 'saturation', 'color', 'luminosity'];
 const stylesPath = ['circle', 'ellipse', 'polygon'];
-
+//переменная с ссылкой на фото
+let pic = prompt('Введите ссылку на картинку', 'https://sun9-east.userapi.com/sun9-17/s/v1/ig2/-hRS1sN0I1EyHsEr2eO4I3DahfvmFH0n7ldR9dHmbiOfegCezOrzABOhh5gmEwfYENZCs2FXkr2cI7w3pIq2CcII.jpg?size=736x920&quality=96&type=album');
+div.style.background = `url('${pic}')`;
+//опция включения рамок
+let option = confirm('Включить появление рамок?');
+//время появления эффекта
+let timShow = prompt('Введите появления эффекта в миллисекундах', 500);
+div.style.transition = `all ${timShow}ms ease`;
 //случайное присвоение блоку цвета, режима наложения и clip-path
-setInterval(() => {
+let changeEffecto = () => {
 	//цвета и режим наложения
 	let colorBack = `rgb(${getRand(0,255)},${getRand(0,255)},${getRand(0,255)})`;
 
@@ -20,16 +27,18 @@ setInterval(() => {
 	div.style.backgroundBlendMode = mode;
 
 	//сlip-path
-	let oneOfPath = getRand(0,2);
-	let path = stylesPath[oneOfPath];
+	if (option == 1) {
+		let oneOfPath = getRand(0,2);
+		let path = stylesPath[oneOfPath];
 
-	if (oneOfPath == 0) {
-		div.style.clipPath = path + `(${getRand(0,100)}% at ${getRand(0,100)}% ${getRand(0,100)}%)`;
+		if (oneOfPath == 0) {
+			div.style.clipPath = path + `(${getRand(0,100)}% at ${getRand(0,100)}% ${getRand(0,100)}%)`;
+		}
+		if (oneOfPath == 1) {
+			div.style.clipPath = path + `(${getRand(0,100)}% ${getRand(0,100)}% at ${getRand(0,100)}% ${getRand(0,100)}%)`;
+		}
+		if (oneOfPath == 2){
+			div.style.clipPath = path + `(${getRand(0,100)}% ${getRand(0,100)}%, ${getRand(0,100)}% ${getRand(0,100)}%, ${getRand(0,100)}% ${getRand(0,100)}%, ${getRand(0,100)}% ${getRand(0,100)}%)`;
+		}
 	}
-	if (oneOfPath == 1) {
-		div.style.clipPath = path + `(${getRand(0,100)}% ${getRand(0,100)}% at ${getRand(0,100)}% ${getRand(0,100)}%)`;
-	}
-	if (oneOfPath == 2){
-		div.style.clipPath = path + `(${getRand(0,100)}% ${getRand(0,100)}%, ${getRand(0,100)}% ${getRand(0,100)}%, ${getRand(0,100)}% ${getRand(0,100)}%, ${getRand(0,100)}% ${getRand(0,100)}%)`;
-	}
-},1000);
+};
